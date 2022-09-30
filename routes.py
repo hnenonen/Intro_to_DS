d28fcd6ef125ddf364f5f0ff081f48c85ba44427
Nenonen Heikki A <nheikki@cubbli22-gold.local>
2022-09-30 14:25:22 +0300

    added error check for input

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from app import app
+import service
 from flask import render_template, request, redirect, session, abort
 
 @app.route("/")
@@ -8,20 +9,8 @@ def index():
 @app.route("/evaluate", methods=["GET","POST"])
 def evaluate():
     text = request.form["inputOverview"]
-    
-
-    worth_watch = "BAD"
-    estimated_runtime = 180 
-    estimated_gross = 9999
-    estimated_voteaverage = 5.0
-
-    # text for analysis 
-    if len(text) > 100:
-        worth_watch = "GOOD"
-        estimated_runtime = 120
-        estimated_gross = 239999
-        estimated_voteaverage = 9.0
-
-
-    
-    return render_template("evaluate.html", text=text, worth_watch = worth_watch, estimated_runtime=estimated_runtime, estimated_gross=estimated_gross, estimated_voteaverage=estimated_voteaverage)
+    if len(text) == 0 or len(text) >10000:
+        return render_template("error.html", message="Overview too short or too long")
+    else:
+        worth_watch, estimated_runtime, estimated_gross, estimated_voteaverage = service.evaluate(text)
+        return render_template("evaluate.html", text=text, worth_watch = worth_watch, estimated_runtime=estimated_runtime, estimated_gross=estimated_gross, estimated_voteaverage=estimated_voteaverage)
--- service.py
+++ service.py
@@ -1,1 +1,14 @@
-asd
+def evaluate(text):
+    worth_watch = "BAD"
+    estimated_runtime = 180 
+    estimated_gross = 9999
+    estimated_voteaverage = 5.0
+
+    # text for analysis 
+    if len(text) > 100:
+        worth_watch = "GOOD"
+        estimated_runtime = 120
+        estimated_gross = 239999
+        estimated_voteaverage = 9.0
+
+    return(worth_watch, estimated_runtime, estimated_gross, estimated_voteaverage)
