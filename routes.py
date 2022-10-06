1edb98e4f12e765402fe6f43c798265d85db042a
Nenonen Heikki A <nheikki@cubbli22-gold.local>
2022-10-06 12:35:12 +0300

    heroku testing

--- routes.py
+++ routes.py
@@ -12,6 +12,11 @@ def evaluate():
     if len(text) == 0 or len(text) >10000:
         return render_template("error.html", message="Overview too short or too long")
     else:
-        worth_watch, estimated_runtime, estimated_gross, estimated_voteaverage = service.evaluate(text)
-        recommendation_list = service.recommendation()
+        worth_watch = "BAD"
+        estimated_runtime = 180 
+        estimated_gross = 9999
+        estimated_voteaverage = 5.0
+        recommendation_list = [84773, "The Lord of the Rings: The Rings of Power", 7]
+        #worth_watch, estimated_runtime, estimated_gross, estimated_voteaverage = service.evaluate(text)
+        #recommendation_list = service.recommendation()
         return render_template("evaluate.html", text=text, worth_watch = worth_watch, recommendation_list = recommendation_list, estimated_runtime=estimated_runtime, estimated_gross=estimated_gross, estimated_voteaverage=estimated_voteaverage)
